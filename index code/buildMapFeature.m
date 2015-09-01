ba2ccda275f8820e4e43ee8fb51d43b3dceebbbb
Christian Radcliffe Ward <christian.radcliffe.ward@gmail.com>
2015-09-01 11:04:44 -0400

    correctly built the sample_screen array

--- index code/buildMapFeature.m
+++ index code/buildMapFeature.m
@@ -2,20 +2,26 @@
 % the sample at the given sample rate
 function result = buildMapFeature(data,sample_rate,window_overlap,feature)
 [channels,duration] = size(data);
+window_size = sample_rate;
 % break data down into windows with desired overlap
 window_shift = 1 - window_overlap/100;
-num_samples = duration / ( sample_rate * window_shift ) + 1;
-sample_screen = zeros(num_samples,duration,channels);
+num_samples = duration / ( window_size * window_shift ) + 1;
+sample_screen = zeros(num_samples,window_size,channels);
+
+% build an array of just the data samples to be passed, this should allow
+% matlab to keep up for processing the initial features. this saves me time
+% from having to deploy feature building functions in C/C++. of course, the
+% mapping will most likely need to be done in C/C++ given the high
+% dimensionality of the data being configured.
 for i=1:num_samples
-    sample_count = (i-1) * window_shift * sample_rate + sample_rate/2;
-    leading_edge = sample_count-sample_rate;
-    if( sample_count < sample_rate )
-        sample_window = [ones(1,sample_count)  zeros(1,duration-sample_count)];
-    elseif( sample_count >= sample_rate && leading_edge <= duration-sample_rate )
-        sample_window = [zeros(1,leading_edge) ones(1,sample_rate) zeros(1,duration-sample_rate-leading_edge)];
+    sample_count = (i-1) * window_shift * window_size + window_size/2;
+    leading_edge = sample_count-window_size;
+    if( sample_count < window_size )
+        sample_screen(i,:,:) = [data(:,1:sample_count)  zeros(1,window_size-sample_count)];
+    elseif( sample_count >= window_size && leading_edge <= duration-window_size )
+        sample_screen(i,:,:) = data(:,leading_edge+1:leading_edge+sample_rate);
     else
-        sample_window = [zeros(1,leading_edge) ones(1,duration-leading_edge)];
+        sample_screen(i,:,:) = [zeros(1,window_size-(duration-leading_edge+1)) data(:,leading_edge:end)];
     end
-    sample_screen(i,:,:) = sample_window;
 end
 end
