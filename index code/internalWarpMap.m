8599c07aba7ac300505cdc595b0955827be81a28
Christian Radcliffe Ward <christian.radcliffe.ward@gmail.com>
2015-09-18 14:15:06 -0400

    commented out the print commands, because i'm keeping plots open between iterations.

--- index code/internalWarpMap.m
+++ index code/internalWarpMap.m
@@ -31,14 +31,14 @@ result_flag( result_full > repmat(temp_mean+temp_std,num_samples,1) ) = 1/3;
 anno_name = ['C:/_ward/_thesis/_Data/physio/eegmmidb/Annotations/' file_name(1:7) '_ANN.ann'];
 [event_tags, anno_listing] = annotationEventTags(anno_name,num_samples,sample_rate,window_overlap);
 
-close all;
-
-figure(20);plot(result_flag(:,20).*anno_listing)
-figure(100);plot(result_flag(:,100).*anno_listing)
-figure(200);plot(result_flag(:,200).*anno_listing)
-figure(300);plot(result_flag(:,300).*anno_listing)
-figure(400);plot(result_flag(:,400).*anno_listing)
-figure(500);plot(result_flag(:,500).*anno_listing)
+% close all;
+% 
+% figure(20);plot(result_flag(:,20).*anno_listing)
+% figure(100);plot(result_flag(:,100).*anno_listing)
+% figure(200);plot(result_flag(:,200).*anno_listing)
+% figure(300);plot(result_flag(:,300).*anno_listing)
+% figure(400);plot(result_flag(:,400).*anno_listing)
+% figure(500);plot(result_flag(:,500).*anno_listing)
 
 save('result.mat','result');
 
