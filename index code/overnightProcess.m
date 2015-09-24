f8517ed2100be365d9adf71eb02cff63f1db9dae
Christian Radcliffe Ward <christian.radcliffe.ward@gmail.com>
2015-09-24 12:32:22 -0400

    fixed error with overnightProcess trying to access files that did not have annotation file

--- index code/overnightProcess.m
+++ index code/overnightProcess.m
@@ -2,10 +2,11 @@ function overnightProcess(directory)
 
 file_listing = dir(directory);
 
-for i=3:length(file_listing)
+for i=1:length(file_listing)
     % operate only on files in the folder, not directories
     [path,name,ext] = fileparts(file_listing(i).name);
-    if( file_listing(i).isdir == 0 && strcmp(ext,'.mat'))
+    if( file_listing(i).isdir == 0 && strcmp(ext,'.mat') && ...
+            ~strcmp(name(6:7),'01') && ~strcmp(name(6:7),'02'))
         file_to_handle = file_listing(i).name;
         % call internal map to compute and save DTW matrix
         file_name = [pwd '\' directory '\' file_to_handle];
