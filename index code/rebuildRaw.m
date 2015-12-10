dced8b75a0d0d7f548b64b50af8f4a7922d61187
Christian Radcliffe Ward <christian.radcliffe.ward@gmail.com>
2015-12-10 16:36:46 -0500

    debugging of rebuildRaw to properly label true window index on x-axis

--- index code/rebuildRaw.m
+++ index code/rebuildRaw.m
@@ -6,10 +6,10 @@ function rebuildRaw(file_name,window,range)
 load(file_name);
 
 overlap = (100-final_output{1}(3))/100;
+% given overlap, how many windows must be ignored to rebuild raw signal
+window_step = 1/overlap;
 
 if(range ~= 0)
-    % given overlap, how many windows must be ignored to rebuild raw signal
-    window_step = 1/overlap;
     half_window = window_step * range;
     rebuilt_raw = zeros(1,2*range*final_output{1}(2));
     window_start = window - half_window;
@@ -19,13 +19,17 @@ if(range ~= 0)
         rebuilt_raw(indx:indx-1+final_output{1}(2)) = ...
             final_output{2}(i*window_step + window_start,:);
     end
+    window_index = window_start:window_step:window_start+2*half_window;
+    xticks = (window_index - window_start)/window_step.*final_output{1}(2);
 else
     % range is zero, find that specific window
     rebuilt_raw = final_output{2}(window,:);
+    xticks = [1 final_output{1}(2)];
+    window_index = [window-2 window+2];
 end
 
 % plot raw data
-window_index = window_start:window_step:window_start+2*half_window;
+
 title_label = ['Window: ' num2str(window) ' Range: ' num2str(range)];
 figure('numbertitle','off','name',['Raw Data: ' num2str(window)])
 plot(rebuilt_raw,'LineWidth',2);
@@ -36,7 +40,6 @@ ylabel('Amplitude (\muV)')
 xlim([0 length(rebuilt_raw)]);
 ax = gca;
 ax.FontSize = 20;
-xticks = (window_index - window_start)/window_step.*final_output{1}(2);
 ax.XTick = xticks;
 ax.XTickLabel = num2cell(window_index);
 end
