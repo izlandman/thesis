0d1fd7cbb1187cd6bbb6831b483a3b892a40c71b
Christian Radcliffe Ward <christian.radcliffe.ward@gmail.com>
2015-12-11 17:06:31 -0500

    Finalized color selection!

--- index code/compareRawFigs.m
+++ index code/compareRawFigs.m
@@ -1,6 +1,6 @@
 file_name = '.\2015_10_15_10_11_26\warp\S001R03_20_.mat';
 window_1 = 94;
-window_2 = 393;
+window_2 = 227;
 range = 2;
 
 result_1 = rebuildRaw(file_name,window_1,range);
@@ -9,7 +9,7 @@ result_2 = rebuildRaw(file_name,window_2,range);
 time = [1:length(result_1)];
 
 figure(42);hold on;
-plot(time,result_1,'Color',[0,0,0],'linewidth',2);
+plot(time,result_1,'Color',[0/255,0/255,0/255],'linewidth',2);
 plot(time,result_2,'Color',[0,153/255,153/255],'linewidth',2);
 xlim([1 max(time)]);
 ylabel('Amplitude (\muV)');
@@ -18,5 +18,5 @@ title('Raw Event Signal Data');
 ax = gca;
 ax.FontSize = 20;
 ax.XTick = 0:40:max(time);
-legend('Event 2', 'Event 6','Location','south','Orientation','horizontal');
+legend('Event 2', 'Event 4','Location','south','Orientation','horizontal');
 grid on;
